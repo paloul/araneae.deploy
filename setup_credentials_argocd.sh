563295af4c95203dade4e7ecd512ca82939c1e16
George K. Paloulian <george.k.paloulian@gmail.com>
2023-04-13 18:21:24 -0700

    Fixed the param naming in the script

--- setup_credentials_argocd.sh
+++ setup_credentials_argocd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 DISTRIBUTION_PATH="./distribution"
 
-git-https-username=${git-https-username:-admin@araneae.io} # The Git repo's https username as default admin@araneae.io
-git-https-password=${git-https-password:-password} # The Git repo's https password as default admin
+githttpsusername=${githttpsusername:-admin} # The Git repo's https username as default admin
+githttpspassword=${githttpspassword:-password} # The Git repo's https password as default admin
 
 while [ $# -gt 0 ]; do
 
    if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
         declare $param="$2"
-        echo $1 $2 // Optional to see the parameter:value result
+        echo $1 $2 # Optional to see the parameter:value result
    fi
 
   shift
