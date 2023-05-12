8a53f1f2e44228d6d090530e3b2c603754643f68
George K. Paloulian <george.k.paloulian@gmail.com>
2023-05-12 13:28:12 -0700

    Added Sealed Secret for mysql root password for the Pipelines module.

--- setup_credentials_pipelines.sh
+++ setup_credentials_pipelines.sh
@@ -2,8 +2,7 @@
 
 # Configure the k8s secrets to access the Kubeflow Pipelines dependencies
 
-# Generate the passwords for admin 'postgres' account and the user for the specific kubeflow dbs
-POSTGRESQL_DB_USER=pipelines
-POSTGRESQL_DB_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
-POSTGRESQL_POSTGRES_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
-kubectl create secret generic -n kubeflow pipelines-postgresql-secret --from-literal=postgres-password=${POSTGRESQL_POSTGRES_PASS} --from-literal=username=${POSTGRESQL_DB_USER} --from-literal=password=${POSTGRESQL_DB_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > ${DISTRIBUTION_PATH}/kubeflow/pipelines/base/resources/postgresql-secret.yaml
+# Generate the passwords
+MYSQL_USER=root
+MYSQL_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+kubectl create secret generic -n kubeflow mysql-secret --from-literal=username=${MYSQL_USER} --from-literal=password=${MYSQL_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > ${DISTRIBUTION_PATH}/kubeflow/pipelines/overlays/rook-ceph/patches/mysql-secret.yaml
