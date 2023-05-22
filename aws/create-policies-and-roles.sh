f838f605adf4a1fd6660a5a6f4b7c5bb6c044bdb
George K. Paloulian <george.k.paloulian@gmail.com>
2023-05-22 13:21:49 -0700

    Added helper script for policy and role creation

---
+++ aws/create-policies-and-roles.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+aws iam create-policy \
+  --profile araneae \
+  --policy-name araneae-lb-controller \
+  --policy-document file://aws/iam-policies/lb-controller-v2_2_0-iam_policy.json
+
+eksctl create iamserviceaccount \
+  --cluster=araneae \
+  --namespace=kube-system \
+  --name=araneae-lb-controller \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/araneae-lb-controller \
+  --override-existing-serviceaccounts \
+  --profile araneae \
+  --approve
+
+aws iam create-policy \
+  --profile araneae \
+  --policy-name dev-araneae-cluster-autoscaler \
+  --policy-document file://aws/iam-policies/cluster-autoscaler-policy.json
+
+eksctl create iamserviceaccount \
+  --cluster=araneae \
+  --namespace=kube-system \
+  --name=dev-araneae-cluster-autoscaler \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/dev-araneae-cluster-autoscaler \
+  --override-existing-serviceaccounts \
+  --profile araneae \
+  --approve
+
+aws iam create-policy \
+  --profile araneae \
+  --policy-name dev-araneae-external-dns \
+  --policy-document file://aws/iam-policies/external-dns-policy.json
+
+eksctl create iamserviceaccount \
+  --cluster=araneae \
+  --namespace=kube-system \
+  --name=dev-araneae-external-dns \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/dev-araneae-external-dns \
+  --override-existing-serviceaccounts \
+  --profile araneae \
+  --approve
+
+aws iam create-policy \
+  --profile araneae \
+  --policy-name dev-araneae-cert-manager \
+  --policy-document file://aws/iam-policies/cert-manager-iam_policy.json
+
+eksctl create iamserviceaccount \
+  --cluster=araneae \
+  --namespace=cert-manager \
+  --name=dev-araneae-cert-manager \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/dev-araneae-cert-manager \
+  --override-existing-serviceaccounts \
+  --profile araneae \
+  --approve
