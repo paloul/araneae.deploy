c537e31da23ae67ae3983a49107141a47e5cdcfa
George K. Paloulian <george.k.paloulian@gmail.com>
2023-05-23 19:03:48 -0700

    Updated helper script to reflect policy name changes

--- aws/create-policies-and-roles.sh
+++ aws/create-policies-and-roles.sh
@@ -16,42 +16,42 @@ eksctl create iamserviceaccount \
 
 aws iam create-policy \
   --profile araneae \
-  --policy-name dev-araneae-cluster-autoscaler \
+  --policy-name araneae-cluster-autoscaler \
   --policy-document file://aws/iam-policies/cluster-autoscaler-policy.json
 
 eksctl create iamserviceaccount \
   --cluster=araneae \
   --namespace=kube-system \
   --name=cluster-autoscaler \
-  --attach-policy-arn=arn:aws:iam::654383687924:policy/dev-araneae-cluster-autoscaler \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/araneae-cluster-autoscaler \
   --override-existing-serviceaccounts \
   --profile araneae \
   --approve
 
 aws iam create-policy \
   --profile araneae \
-  --policy-name dev-araneae-external-dns \
+  --policy-name araneae-external-dns \
   --policy-document file://aws/iam-policies/external-dns-policy.json
 
 eksctl create iamserviceaccount \
   --cluster=araneae \
   --namespace=kube-system \
   --name=external-dns \
-  --attach-policy-arn=arn:aws:iam::654383687924:policy/dev-araneae-external-dns \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/araneae-external-dns \
   --override-existing-serviceaccounts \
   --profile araneae \
   --approve
 
 aws iam create-policy \
   --profile araneae \
-  --policy-name dev-araneae-cert-manager \
+  --policy-name araneae-cert-manager \
   --policy-document file://aws/iam-policies/cert-manager-iam_policy.json
 
 eksctl create iamserviceaccount \
   --cluster=araneae \
   --namespace=cert-manager \
   --name=cert-manager \
-  --attach-policy-arn=arn:aws:iam::654383687924:policy/dev-araneae-cert-manager \
+  --attach-policy-arn=arn:aws:iam::654383687924:policy/araneae-cert-manager \
   --override-existing-serviceaccounts \
   --profile araneae \
   --approve
