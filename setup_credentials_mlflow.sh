21df746f0b65248b025de56c8977748cb859ca17
George K. Paloulian <george.k.paloulian@gmail.com>
2023-06-22 16:22:38 -0700

    Trying without helm chart. Just took mlflow image from the chart

--- setup_credentials_mlflow.sh
+++ setup_credentials_mlflow.sh
@@ -6,9 +6,9 @@ DISTRIBUTION_PATH="./distribution"
 # Generate access keys for Minio credentials
 MINIO_ACCESS_KEY=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
 MINIO_SECRET_KEY=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
-kubectl create secret generic -n mlflow mlflow-minio-secret --from-literal=accesskey=${MINIO_ACCESS_KEY} --from-literal=secretkey=${MINIO_SECRET_KEY} --dry-run=client -o yaml | kubeseal | yq eval -P > ${DISTRIBUTION_PATH}/mlflow/minio/minio-sealedsecret.yaml
+kubectl create secret generic -n mlflow mlflow-minio-secret --from-literal=AWS_ACCESS_KEY_ID=${MINIO_ACCESS_KEY} --from-literal=AWS_SECRET_ACCESS_KEY=${MINIO_SECRET_KEY} --dry-run=client -o yaml | kubeseal | yq eval -P > ${DISTRIBUTION_PATH}/mlflow/minio/minio-sealedsecret.yaml
 
 # Generate the root password for MySQL
 MYSQL_USER=root
 MYSQL_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
-kubectl create secret generic -n mlflow mlflow-mysql-secret --from-literal=username=${MYSQL_USER} --from-literal=password=${MYSQL_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > ${DISTRIBUTION_PATH}/mlflow/mysql/mysql-sealedsecret.yaml
+kubectl create secret generic -n mlflow mlflow-mysql-secret --from-literal=MYSQL_USERNAME=${MYSQL_USER} --from-literal=MYSQL_PWD=${MYSQL_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > ${DISTRIBUTION_PATH}/mlflow/mysql/mysql-sealedsecret.yaml
